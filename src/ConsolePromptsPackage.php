b1ffb0ad0bf2b6eb0988e3780b001730aff6b392
Patrick Morpey <patrick@morphp.nl>
2024-08-07 11:19:40 +0200

    Replaced placeholders

--- src/ConsolePromptsPackage.php
+++ src/ConsolePromptsPackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\ConsolePrompts;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class ConsolePromptsPackage extends BasePackage
 {
     use AsSingleton;
 
