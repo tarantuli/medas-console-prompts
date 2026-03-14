0908eff015a9e3875c22ee135a8ebe53a0d5f433
Patrick Morpey <patrick@morphp.nl>
2026-03-14 11:06:18 +0100

    Updated dependencies and optimized composer configuration

--- src/ConsolePromptsPackage.php
+++ src/ConsolePromptsPackage.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\ConsolePrompts;
 
-use Medas\Core\AsSingleton;
-use Medas\ServiceManager\BasePackage;
+use Medas\Core\{AsSingleton, BasePackage};
 
 class ConsolePromptsPackage extends BasePackage
 {
